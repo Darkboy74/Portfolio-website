aa00450dfab44b826646ba5c2b3855c558fed5ff
Darkboy74 <danielbobola903@gmail.com>
2025-06-28 18:27:34 +0100

    Add files via upload

---
+++ projects/2/script.js
@@ -0,0 +1,17 @@
+function calculate() {
+    let num1 = parseFloat(document.getElementById('num1').value);
+    let num2 = parseFloat(document.getElementById('num2').value);
+    let operator = document.getElementById('operator').value;
+    let result;
+  
+    switch (operator) {
+      case '+': result = num1 + num2; break;
+      case '-': result = num1 - num2; break;
+      case '*': result = num1 * num2; break;
+      case '/': result = num1 / num2; break;
+      default: result = 'Invalid Operation';
+    }
+  
+    document.getElementById('result').innerText = `Result: ${result}`;
+  }
+  
